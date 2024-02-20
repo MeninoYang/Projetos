efc4e39569af5fadb4053728deeca7a26008e6d6
MeninoYang <154245177+MeninoYang@users.noreply.github.com>
2024-02-20 13:15:11 -0300

    Delete Pacote directory

--- Pacote/Graficos/plotagem.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-def plotar_grafico(x, y, titulo, xlabel, ylabel):
-    plt.plot(x, y)
-    plt.title(titulo)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.show()
